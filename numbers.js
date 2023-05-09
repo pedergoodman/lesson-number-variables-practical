460f1c10e60f4de5ee12d1b00cf5718b913da9aa
Peder Goodman <peder.goodman@gmail.com>
2023-05-08 20:25:54 -0500

    Followed along with video

---
+++ numbers.js
@@ -0,0 +1,43 @@
+console.log( 'js' );
+
+let motorcycles = 9;
+let cars = 0;
+let vehicles = motorcycles + cars; 
+
+
+console.log( 'bikes', motorcycles );
+console.log( 'cars', cars );
+console.log( 'total vehicles', vehicles );
+
+console.log( 'had to put the soft tail in the garage' );
+
+//decrement motorcycles
+motorcycles--; // decrements by 1 
+console.log( 'bikes', motorcycles);
+console.log( 'cars', cars );
+vehicles = motorcycles + cars;
+console.log( 'total vehicles', vehicles );
+console.log( 'missed the bike so I bought 2 Lambos' );
+cars += 2; // adds itslef plus 2
+
+console.log( 'cars', cars );
+console.log( 'bikes', motorcycles);
+vehicles = motorcycles + cars;
+console.log( 'total vehicles', vehicles );
+
+console.log( 'wanted a Jetta, so I got one' );
+cars++; // increments by 1
+console.log( 'sold 3 bikes' );
+motorcycles -= 3; // subtracts itself - 3 (or other number)
+console.log( 'bikes', motorcycles);
+console.log( 'cars', cars );
+vehicles = motorcycles + cars;
+console.log( 'total vehicles', vehicles );
+
+console.log( 'had to put the soft tail in the garage' );
+motorcycles++
+
+console.log( 'bikes', motorcycles);
+console.log( 'cars', cars );
+vehicles = motorcycles + cars;
+console.log( 'total vehicles', vehicles );
